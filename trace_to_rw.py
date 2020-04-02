c6ef0aa58f6a777d6a3e6adec6010b302ac929a0
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-04-02 18:29:32 -0300

    Add TZ cand list and implement TraceProcessor methods

--- trace_to_rw.py
+++ trace_to_rw.py
@@ -1,9 +1,10 @@
 class TraceProcessor(object):
     def __init__(self):
-        self.cu_position = tuple([0, 0])
+        self.cu_position = tuple()
         self.ref_frame = str()
+        self.cu_size = str()
 
-        self.TRACE_DICT = {
+        self.trace_dict = {
             "U ": self.process_cu,
             "P ": self.process_pu,
             "C ": self.process_block,
@@ -12,26 +13,38 @@ class TraceProcessor(object):
         }
 
     def trace_to_rw(self, trace_path):
-        i = 0
-
         with open(trace_path) as trace:
             for line in trace:
                 key = line[0:2]
-                if key in self.TRACE_DICT:
-                    self.TRACE_DICT[key](line)
-
-                if i == 50:
-                    break
-                i += 1
-
-    def process_pu(self, line):
-        print("processamento de uma PU")
+                if key in self.trace_dict:
+                    self.trace_dict[key](line)
 
     def process_cu(self, line):
-        print("início da codificação de uma CU")
+        # U <xCU> <yCU> <size>
+        _, x, y, cu_size = line.split()
+
+        self.cu_size = cu_size
+        self.cu_position = (x, y)
+
+    def process_pu(self, line):
+        # P <sizePU> <idPart> <ref_frame_id>
+        _, size_pu, id_part, ref_frame = line.split()
+
+        self.ref_frame = ref_frame
 
     def process_block(self, line):
-        print("acesso a um bloco candidato")
+        # C <xCand> <yCand>
+        _, x, y = line.split()
+
+        initial_x = int(x) + int(self.cu_position[0])
+        initial_y = int(y) + int(self.cu_position[1])
+
+        final_x = initial_x + int(self.cu_size)
+        final_y = initial_y + int(self.cu_size)
+
+        for i in range(initial_y, final_y):
+            for j in range(initial_x, final_x):
+                print(f"R {i} {j} {self.ref_frame}")
 
     def process_first_search(self, line):
         print("posição central da first search")
